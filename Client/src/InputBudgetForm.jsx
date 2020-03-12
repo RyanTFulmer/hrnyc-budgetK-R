6b4817e5f8e394e23f4ef5585002b4b65d506dab
kqiu10 <qiukan1011@126.com>
2020-03-12 14:41:04 -0400

    finsih transaction part

--- Client/src/InputBudgetForm.jsx
+++ Client/src/InputBudgetForm.jsx
@@ -8,20 +8,7 @@ const InputBudgetForm = props => {
           Month:
           <input type="text" name="month" onChange={props.handleBudgetChange} />
         </label>
-        {props.transactionTypes.map(newCategory => {
-          return (
-            <div>
-              <label>
-                {newCategory}
-                <input
-                  type="number"
-                  name={newCategory}
-                  onChange={props.handleBudgetChange}
-                />
-              </label>
-            </div>
-          );
-        })}
+
       </form>
       <button onClick={props.handleBudgetSubmit}>Submit budget</button>
     </div>
@@ -29,3 +16,18 @@ const InputBudgetForm = props => {
 };
 
 module.exports = InputBudgetForm;
+
+// {props.transactionTypes.map(newCategory => {
+//   return (
+//     <div>
+//       <label>
+//         {newCategory}
+//         <input
+//           type="number"
+//           name={newCategory}
+//           onChange={props.handleBudgetChange}
+//         />
+//       </label>
+//     </div>
+//   );
+// })}
